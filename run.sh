a3e4e6a29040b06480d7c1104cb3ac8382906e47
Chamon <alexchamon@gmail.com>
2018-07-30 01:30:58 -0300

    Fixing verification on run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,28 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH="$DOTFILES_DIR/bin:$PATH"
 
 ## Update dotfiles itself
-if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+echo Updating dotfiles...
+if type "git -a -d "$DOTFILES_DIR/.git"" 2>/dev/null; then 
+    git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; 
+fi
 
 ## Verify if vim is installed
-if hash vim 2>/dev/null; then
+echo Verifying if vim is installed...
+if type "vim" 2>/dev/null; then
     sudo apt install vim
+else
+    echo Vim is installed!
 fi
 
 ## Verify if Vundle is installed
+echo Verifying if Vundle is installed...
 if [ ! -d "~/.vim/bundle/" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+    echo Vundle is installed!
 fi
 
+
+echo Soft-linking files
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~

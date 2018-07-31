b9741a479677fa56f063e46d042dca3450f8cabd
Chamon <alexchamon@gmail.com>
2018-07-31 02:09:52 -0300

    Adding tmux and vim color scheme

--- run.sh
+++ run.sh
@@ -7,17 +7,20 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 ## Update dotfiles itself
 echo Updating dotfiles...
-if type "git -a -d "$DOTFILES_DIR/.git"" 2>/dev/null; then 
+if [ type "git -a -d "$DOTFILES_DIR/.git"" 2>/dev/null] ; then 
     git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; 
 fi
 
 ## Verify if vim is installed
 echo Verifying if vim is installed...
-if type "vim" 2>/dev/null; then
+if [ type "vim" 2>/dev/null ]; then
     sudo apt install vim
 else
     echo Vim is installed!
 fi
+
+## Install vim color scheme
+cp "$DOTFILES_DIR/colors/*.vim" ~/.vim/colors
 
 ## Verify if Vundle is installed
 echo Verifying if Vundle is installed...
@@ -27,7 +30,14 @@ else
     echo Vundle is installed!
 fi
 
+## Verify if Tmux is installed
+echo Verifying if tmux is installed...
+if [ type "tmux" 2>/dev/null ];then
+    sudo apt install tmux
+else
+    echo tmux is installed!
 
 echo Soft-linking files
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
